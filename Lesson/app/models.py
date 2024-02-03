09bf06d36193be34eb3f56bac5219908b2025900
X9alone <dimakadusko@gmail.com>
2024-02-03 15:55:27 +0100

    add navbar, form_new_contacts, grid

--- Lesson/app/models.py
+++ Lesson/app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Contact(models.Model):
+    name = models.CharField('name', max_length=20)
+    last_name = models.CharField('last_name', max_length=20)
+    number = models.CharField('number', max_length=15)
+    photo = models.FileField('photo', upload_to='uploads/', null=True,
+                             blank=True)
