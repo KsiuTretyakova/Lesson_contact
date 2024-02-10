663b147ad595d7bdae59b72deb996e0ad2b9969d
ShadowOfSokol <sokolenkoegor19702002@gmail.com>
2024-02-10 15:44:01 +0200

    Add Class Contact

--- Lesson/app/models.py
+++ Lesson/app/models.py
@@ -4,3 +4,6 @@ class Contact(models.Model):
     name = models.CharField('name', max_length=20)
     last_name = models.CharField('last_name', max_length=20)
     number = models.CharField('number', max_length=15)
+
+    def __str__(self):
+        return f'{self.name} {self.number}'
