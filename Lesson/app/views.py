497177b633d0f8c9b16b3598af502ab224c2266b
Arystarkh <kruftik203@gmail.com>
2024-02-17 16:46:42 +0200

    Dispalying photos

--- Lesson/app/views.py
+++ Lesson/app/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Contact
 
-
-name = 'Arystarkh'
 
 def index(request):
+    contacts = Contact.objects.all()
     return render(
         request,
         'main/index.html',
-        {'nameVariable': name, 'title': 'Contacts list'}
+        {'data': contacts, 'title': 'Contacts list'}
     )
