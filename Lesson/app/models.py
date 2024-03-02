21264086751e5286ce8def71bf83ec0622f1b2a2
Mitz <teamkillersboost@gmail.com>
2024-03-02 16:36:53 +0300

    останны змыни

--- Lesson/app/models.py
+++ Lesson/app/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Contact(models.Model):
+    name=models.CharField('name', max_length=20)
+    last_name=models.CharField('last_name', max_length=20)
+    number=models.CharField('number', max_length=14)
