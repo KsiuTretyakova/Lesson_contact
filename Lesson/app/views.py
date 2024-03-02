c49cea86fba208a5e3085216cbab3007a6027b5e
Arystarkh <kruftik203@gmail.com>
2024-03-02 16:48:23 +0200

    returned old form, added functional to "add" button

--- Lesson/app/views.py
+++ Lesson/app/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Contact
+from .forms import ContacForm
 
 
 def index(request):
@@ -11,5 +12,15 @@ def index(request):
         {'data': contacts, 'title': 'Contacts list'}
     )
 
-def newContact():
-    pass
+def addContact(request):
+    error =  ""
+    if request.method == "POST":
+        form = ContacForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect(index)
+        else:
+            error = "ERROR"
+    form = ContacForm()
+    return(request, 'main/index.html',
+           {'form': form, 'error': error})
