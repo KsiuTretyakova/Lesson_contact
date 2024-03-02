03ff72bd2b611200474dc626658f45a876f4e981
MaxMozgunov <136356747+MaxMozgunov@users.noreply.github.com>
2024-03-02 16:07:04 +0100

    Ошибки больше нет.

--- Lesson/app/forms.py
+++ Lesson/app/forms.py
@@ -1,13 +1,12 @@
 from .models import Contact
 from django import forms
 
-
 class ContactForm(forms.ModelForm):
     class Meta:
         model = Contact
         fields = ["name", "last_name", "number", "photo"]
-        wigets = {"name": forms.TextInput(attes={"class": "form-control"}),
-                  "last_name": forms.TextInput(attes={"class": "form-control"}),
-                  "number": forms.TextInput(attes={"class": "form-control"}),
+        widgets = {"name": forms.TextInput(attrs={"class": "form-control"}),
+                  "last_name": forms.TextInput(attrs={"class": "form-control"}),
+                  "number": forms.TextInput(attrs={"class": "form-control"}),
                   "photo": forms.FileInput()
                   }
