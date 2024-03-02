97acc940907d241cd6787061c7768034d41ee1ed
KsiuTretyakova <80200657+KsiuTretyakova@users.noreply.github.com>
2024-03-02 14:32:25 +0000

    Активація форми

--- Lesson/app/views.py
+++ Lesson/app/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Contact
+from .forms import ContactForm
 
 
 def index(request):
@@ -11,5 +12,15 @@ def index(request):
         {'data': contacts, 'title': 'Contacts list'}
     )
 
-def addContact():
-    pass
+def addContact(request):
+    error = ""
+    if request.method == "POST":
+        form = ContactForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect(index)
+        else:
+            error = "ERROR"
+    form = ContactForm()
+    return (request, 'main/index.html', 
+            {'form':form, 'error':error})
