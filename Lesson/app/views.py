3f1b81c81464749220eb7bca25ba5f24f3449fe3
MaxMozgunov <136356747+MaxMozgunov@users.noreply.github.com>
2024-03-02 15:56:44 +0100

    add text in function "addContact"

--- Lesson/app/views.py
+++ Lesson/app/views.py
@@ -8,7 +8,7 @@ from django.http import HttpResponse
 def index(request):
     contacts = Contact.objects.all()
     print(contacts)
-    return render(request, "index.html", {'data': contacts})
+    return render(request, "main/index.html", {'data': contacts, 'form': ContactForm()})
 
 
 def addContact(request):
