b527f033fcf94ce514fbe81434732aa48159a019
Shulga M <maxim.shulga2008@gmail.com>
2024-03-16 16:59:00 +0200

    added deleteContact function added editContact function doesn't work need to fix

--- Lesson/Lesson/urls.py
+++ Lesson/Lesson/urls.py
@@ -7,7 +7,10 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='home'),
-] + static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
+    path('addContact', views.addContact),
+    path('deleteContact', views.deleteContact),
+    path('editContact', views.editContact)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- Lesson/app/views.py
+++ Lesson/app/views.py
@@ -11,7 +11,7 @@ def index(request):
     return render(
         request,
         'main/index.html',
-        {'data': contacts, 'title': 'Contacts list'}
+        {'data': contacts, 'title': 'Contacts list', 'form': ContactForm()}
     )
 
 def addContact(request):
@@ -24,4 +24,34 @@ def addContact(request):
         else:
             error = "ERROR"
     form = ContactForm()
-    return (request, 'main/index.html', {'form':form, 'error':error})
+    return render(request, 'main/index.html', {'form':form, 'error':error})
+
+
+def deleteContact(request):
+    error = ""
+    if request.method == "POST":
+        try:
+            object = Contact.objects.get(phone_number=request.POST.__getitem__("phone_number"))
+            object.delete()
+        except:
+            return index(request)
+
+    return index(request)
+
+
+def editContact(request):
+    error = ""
+    if request.method == "POST":
+        try:
+            object = Contact.objects.get(phone_number=request.POST.__getitem__("phone_number"))
+            object.delete()
+        except:
+            return index(request)
+        form = ContactForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect(index)
+        else:
+            error = "ERROR"
+    return index(request)
+    
